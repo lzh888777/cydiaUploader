87eb414fe3e2e4e1ce5f406adba98170fb4c91ba
liuzhihua <lzh7373@163.com>
2022-02-14 10:47:18 +0800

    add git file

--- buildPackage.sh
+++ buildPackage.sh
@@ -8,4 +8,5 @@ dpkg-scanpackages debs /dev/null > Packages
 tar zcvf Packages.gz Packages
 bzip2 -k Packages Packages.bz2
 find . -name '*.DS_Store' -type f -delete
-scp -r $curPath root@101.34.200.141:/root/liuzhihua/
+# scp -r $curPath root@101.34.200.141:/root/liuzhihua/
+ssh root@101.34.200.141 'bash -s' < /root/liuzhihua/cydia/git_command.sh
