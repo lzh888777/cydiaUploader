5b024cf213b6db1cc1fee30a0fba1ea956ba560a
liuzhihua <lzh7373@163.com>
2022-02-14 11:40:28 +0800

    update

--- buildPackage.sh
+++ buildPackage.sh
@@ -8,6 +8,10 @@ dpkg-scanpackages debs /dev/null > Packages
 tar zcvf Packages.gz Packages
 bzip2 -k Packages Packages.bz2
 find . -name '*.DS_Store' -type f -delete
+export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890
+git add .
+git commit -m 'update'
+git push
 # scp -r $curPath root@101.34.200.141:/root/liuzhihua/
 # ssh root@101.34.200.141 'bash -s' < $curPath/git_command.sh
 ssh root@101.34.200.141 > /dev/null 2>&1 << eeooff
--- git_command.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd /root/liuzhihua/cydia
-git pull
