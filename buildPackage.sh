bcc01fe68d044992b6e78f0f8477ea7ca45cb0e4
x <l>
2022-02-14 10:59:42 +0800

    merge

--- buildPackage.sh
+++ buildPackage.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-
+curPath=$(cd "$(dirname $0)";pwd)
+echo $curPath
 rm -f Packages
 rm -f Packages.gz
 rm -f Packages.bz2
@@ -7,4 +8,4 @@ dpkg-scanpackages debs /dev/null > Packages
 tar zcvf Packages.gz Packages
 bzip2 -k Packages Packages.bz2
 find . -name '*.DS_Store' -type f -delete
-scp -r /Users/liuzhihua/Desktop/cydia root@101.34.200.141:/root/liuzhihua/
+scp -r $curPath root@101.34.200.141:/root/liuzhihua/
